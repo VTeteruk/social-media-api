457497ae9c3a907af40c3f8d5c5ae2a115838180
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-16 15:04:56 +0300

    Add models: post, follows for social_media app & make migrations

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+
+
+class Follows(models.Model):
+    followers = models.ForeignKey(
+        get_user_model(), on_delete=models.DO_NOTHING, related_name="followers"
+    )
+    following = models.ForeignKey(
+        get_user_model(), on_delete=models.DO_NOTHING, related_name="following"
+    )
