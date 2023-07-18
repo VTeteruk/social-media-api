704392481597ad9d443c1b9bf81af1c45a15db0b
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-18 10:01:49 +0300

    Change post model: add image field & improve others

--- social_media/models.py
+++ social_media/models.py
@@ -1,19 +1,30 @@
+import os
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
 
+def get_unique_pass(instance, filename):
+    _, ext = os.path.splitext(filename)
+
+    filename = (f"{instance.creator.email}"
+                f"-{uuid.uuid4()}"
+                f"{ext}")
+
+    return os.path.join("uploads", "posts", filename)
+
+
 class Post(models.Model):
-    creator = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, null=True, blank=True
-    )
+    creator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(
         "Product Name",
         max_length=100,
-        default="",
-        help_text="This is the help text",
+        default="New post",
     )
     content = models.TextField(
         verbose_name="Content", max_length=600, blank=True
     )
+    image = models.ImageField(null=True, upload_to=get_unique_pass)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
